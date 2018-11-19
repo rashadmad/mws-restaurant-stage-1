4b72ec86e64e9afa24cf70e1e6ec9458f43e73ac
Rashad J Madison <rashadmad@gmail.com>
2018-11-18 23:16:58 -0600

    Onset of refactoring picture list system

--- js/main.js
+++ js/main.js
@@ -156,31 +156,31 @@ fillRestaurantsHTML = (restaurants = self.restaurants) => {
  * Create restaurant HTML.
  */
 createRestaurantHTML = (restaurant) => {
-  const li = document.createElement('li');
+  const NEWDIV = document.createElement('div');
 
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
-  li.append(image);
+  NEWDIV.append(image);
 
   const name = document.createElement('h1');
   name.innerHTML = restaurant.name;
-  li.append(name);
+  NEWDIV.append(name);
 
   const neighborhood = document.createElement('p');
   neighborhood.innerHTML = restaurant.neighborhood;
-  li.append(neighborhood);
+  NEWDIV.append(neighborhood);
 
   const address = document.createElement('p');
   address.innerHTML = restaurant.address;
-  li.append(address);
+  NEWDIV.append(address);
 
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
-  li.append(more)
+  NEWDIV.append(more)
 
-  return li
+  return NEWDIV
 }
 
 /**
