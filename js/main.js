e6042ec6125c1dc24105505c5c4b4e0ee1ebee43
Rashad J Madison <rashadmad@gmail.com>
2018-11-21 03:11:45 -0600

    add breakpoints

--- js/main.js
+++ js/main.js
@@ -166,22 +166,24 @@ createRestaurantHTML = (restaurant) => {
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
 
+  const infoDiv = document.createElement('div');
   const name = document.createElement('h1');
-  name.innerHTML = restaurant.name;
-  li.append(name);
-
-  const neighborhood = document.createElement('p');
-  neighborhood.innerHTML = restaurant.neighborhood;
-  li.append(neighborhood);
-
-  const address = document.createElement('p');
-  address.innerHTML = restaurant.address;
-  li.append(address);
-
   const more = document.createElement('a');
+  const neighborhood = document.createElement('p');
+  const address = document.createElement('p');
+
+  infoDiv.classList.add("info");
+  name.innerHTML = restaurant.name;
+  infoDiv.append(name);
+  neighborhood.innerHTML = restaurant.neighborhood;
+  infoDiv.append(neighborhood);
+  address.innerHTML = restaurant.address;
+  infoDiv.append(address);
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
-  li.append(more)
+  li.append(infoDiv);
+  more.classList.add("li-button");
+  li.append(more);
 
   return li
 }
