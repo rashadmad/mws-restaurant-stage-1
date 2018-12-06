08c5074878c2884d5d84837f8c94557f0c3d8e90
Rashad J Madison <rashadmad@gmail.com>
2018-12-06 14:57:17 -0600

    Add grid system to reviews items

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -151,16 +151,19 @@ createReviewHTML = (review) => {
   name.innerHTML = review.name;
   li.appendChild(name);
 
-  const date = document.createElement('p');
-  date.innerHTML = review.date;
-  li.appendChild(date);
-
   const rating = document.createElement('p');
   rating.innerHTML = `Rating: ${review.rating}`;
+  rating.classList.add("rating");
   li.appendChild(rating);
+
+  const date = document.createElement('p');
+  date.innerHTML = review.date;
+  date.classList.add("date");
+  li.appendChild(date);
 
   const comments = document.createElement('p');
   comments.innerHTML = review.comments;
+  comments.classList.add("comments");
   li.appendChild(comments);
 
   return li;
