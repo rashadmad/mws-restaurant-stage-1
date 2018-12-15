58493a7b77113ad3b75740c7642dca7082c43390
rashad <rashadmad@gmail.com>
2018-12-15 03:16:05 -0600

    gulpe serve errors

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,17 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const log = require('fancy-log');
 const prefixer = require('gulp-autoprefixer');
+const browserSync = require('browser-sync').create();
 
 const sassFiles = 'scss/**/*.scss';
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: '/mws-restaurant-stage-1'
+    },
+  })
+})
 
 gulp.task('styles', function(){
   return gulp.src(sassFiles)
@@ -14,11 +23,15 @@ gulp.task('styles', function(){
             cascade: false
         }))
     .pipe(gulp.dest('css'))
-    log("css file updated");
+    .pipe(browserSync.reload({
+      stream: true
+    }))
 });
 
-gulp.task('default', function(done) {
-  gulp.task('watch', function() {
-      gulp.watch(sassFiles, gulp.series('styles'));
-  });
+gulp.task('watch', function() {
+    gulp.watch(sassFiles, gulp.series('styles'));
 });
+
+gulp.task('default', gulp.parallel('watch','browserSync', function() {
+  log("css file updated");
+}));
