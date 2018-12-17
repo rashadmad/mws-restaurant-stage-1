d966b2b4ab87581d7d04bc18deb4168826902d5e
rashad Madison <rashadmadison@rashads-macbook.attlocal.net>
2018-12-17 13:19:54 -0600

    Continued to refractor css

--- js/main.js
+++ js/main.js
@@ -179,7 +179,6 @@ createRestaurantHTML = (restaurant) => {
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
   li.append(infoDiv);
-  more.classList.add("li-button");
   li.append(more);
 
   return li
