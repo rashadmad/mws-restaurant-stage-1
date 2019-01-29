228e190329a843adac7fb97362c8f2045ae23436
rashad <rashadmad@gmail.com>
2019-01-28 20:38:20 -0600

    third Heroku commit

--- index.js
+++ index.js
@@ -2,9 +2,5 @@ const server = require('server');
 const { get, post } = server.router;
 
 server({ port: process.env.PORT || 8000 }, [
-  get('/', () => render('index.html')),
-  post('/', ctx => {
-    console.log(ctx.data);
-    return redirect('/');
-  })
+  get('/', () => render('index.html'))
 ]);
