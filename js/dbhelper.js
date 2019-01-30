2dde862f8a5b746e1eb960214fd227517905b15b
rashad <rashadmad@gmail.com>
2019-01-30 12:49:11 -0600

    url update

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -150,7 +150,7 @@ class DBHelper {
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant) {
-    return (`mws-restaurant-stage-1/img/${restaurant.photograph}`);
+    return (`/mws-restaurant-stage-1/img/${restaurant.photograph}`);
   }
 
   /**
